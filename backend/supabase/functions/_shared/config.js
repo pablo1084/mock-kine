06b289f917acd649d6e62f5d9736e1a55a3470fa
Pablo Navarro <pablonav84@gmail.com>
2026-09-16 10:41:37 -0300

    reemplazo enlace youtube

--- backend/supabase/functions/_shared/config.js
+++ backend/supabase/functions/_shared/config.js
@@ -1,1 +1,1 @@
-export const DEFAULT_CHANNEL_URL = 'https://www.youtube.com/@kinesiologiadeportiva8487/videos';
+export const DEFAULT_CHANNEL_URL = 'https://www.youtube.com/@Kinesiolog%C3%ADadeportivaJos%C3%A9Ovied';
